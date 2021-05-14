383e22956df33bc0fd169592a4fc6ce62774f705
gabrielkeppen <gabrielkeppen@gmail.com>
2021-05-14 15:36:33 -0300

    Base robo

---
+++ index.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer-core');
+  
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+      });
+    const page = await browser.newPage();
+    await page.goto('https://app.hangdocs.com/login');
+    await page.type('#inputemail','andre.calvo@ovrvu.com.br')
+    await page.waitForTimeout(2000)
+    await page.type('#inputpassword','123mudar')
+    await page.waitForTimeout(2000)
+    await page.click('button[type="submit"]')
+    await page.waitForTimeout(5000)
+    await page.screenshot({ path: 'GFG.png' });
+    await page.waitForTimeout(2000)
+    await browser.close();
+    
+})();
